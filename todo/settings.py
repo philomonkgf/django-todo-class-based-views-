337f6aec2e4f2f90b29be97c28efc259d137c45f
philomonkgf <109361238+philomonkgf@users.noreply.github.com>
2024-03-23 01:16:30 +0530

    Update settings.py
    change port

--- todo/settings.py
+++ todo/settings.py
@@ -136,7 +136,7 @@ LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 000
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'emailid'
 EMAIL_HOST_PASSWORD = 'password'
